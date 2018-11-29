c208b7843337f13ffd62cf350ffe9c4f6fc71e7a
max ortman <mortman2299@stu.d214.org>
2018-11-29 15:06:47 -0600

    e

---
+++ word play/ThrirdViewControler.swift
@@ -0,0 +1,18 @@
+//
+//  ThrirdViewControler.swift
+//  word play
+//
+//  Created by max ortman on 11/29/18.
+//  Copyright © 2018 john hersey high school. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewControler: UIViewController {
+    
+    var TTtext = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
